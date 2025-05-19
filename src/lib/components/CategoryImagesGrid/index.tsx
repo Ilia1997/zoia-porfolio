51e0c3c19ce2ce0fa453709ac11a99e038c35773
IlunhaWirebee <ilunha@wirebee.com>
2025-05-19 22:51:59 +0200

    fix: refactor CategoryImagesGrid layout for improved responsiveness and structure

--- src/lib/components/CategoryImagesGrid/index.tsx
+++ src/lib/components/CategoryImagesGrid/index.tsx
@@ -2,6 +2,8 @@
 import { cn } from "@/lib/components/utils";
 import { urlFor } from "@/sanity/client";
 import { CategoryImagesItem } from "@/types";
+import Image from "next/image";
+import { Fragment } from "react";
 import ReactCompareImage from "react-compare-image";
 
 export const CategoryImagesGrid = ({
@@ -10,25 +12,45 @@ export const CategoryImagesGrid = ({
   categoryItems: CategoryImagesItem[];
 }) => {
   return (
-    <div className="grid md:grid-cols-2 gap-3 mt-8 md:mt-14">
-      {categoryItems.map((item) => (
-        <div
-          key={item._id}
-          className={cn(
-            "relative md:min-h-[100lvh] min-h-[600px] category-item-image-slider",
-            item.isWidescreen ? "col-span-full" : ""
-          )}
-        >
-          <ImageSlider item={item} />
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="md:grid-cols-2 gap-3 mt-8 md:mt-14 hidden lg:grid">
+        {categoryItems.map((item) => (
+          <div
+            key={item._id}
+            className={cn(
+              "relative md:min-h-[100lvh] min-h-[600px] category-item-image-slider",
+              item.isWidescreen ? "col-span-full" : ""
+            )}
+          >
+            <ImageSlider item={item} />
+          </div>
+        ))}
+      </div>
+      <div className="gap-3 mt-8 md:mt-14 flex flex-wrap lg:hidden">
+        {categoryItems.map((item) => (
+          <Fragment key={item._id}>
+            <CategoryImageItem
+              sanityImageUrl={item.beforeImage}
+              title="image-1"
+            />
+            <CategoryImageItem
+              sanityImageUrl={item.afterImage}
+              title="image-2"
+            />
+          </Fragment>
+        ))}
+      </div>
+    </>
   );
 };
 
 const ImageSlider = ({ item }: { item: CategoryImagesItem }) => {
   const imageBefore = urlFor(item.beforeImage).width(4500).url();
   const imageAfter = urlFor(item.afterImage).width(4500).url();
+
+  if (typeof window !== "undefined" && window?.innerWidth < 1024) {
+    return null;
+  }
 
   return (
     <ReactCompareImage
@@ -38,5 +60,28 @@ const ImageSlider = ({ item }: { item: CategoryImagesItem }) => {
       rightImageCss={{ height: "100%" }}
       skeleton={<div className="h-full w-full bg-gray-200 animate-pulse" />}
     />
+  );
+};
+
+const CategoryImageItem = ({
+  sanityImageUrl,
+  title,
+}: {
+  sanityImageUrl: { _type: string; asset: { _ref: string; _type: string } };
+  title: string;
+}) => {
+  const imageUrl = urlFor(sanityImageUrl).width(4500).url();
+  return (
+    <div className="relative w-full">
+      <Image
+        placeholder="blur"
+        src={imageUrl}
+        blurDataURL={urlFor(sanityImageUrl).width(10).url()}
+        alt={title}
+        priority={true}
+        width={3500}
+        height={3500}
+      />
+    </div>
   );
 };
